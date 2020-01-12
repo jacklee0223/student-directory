d06aefe62e890f5337f89832611702c9d7e4707d
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-11 21:37:14 -0600

    reorganize code

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const morgan = require('morgan');
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// DB setup
+mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true });
+
+// Middlewares
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
